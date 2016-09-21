3f89b015bef24aa8203d6ad2f359f711a4b2ca42
Tom Brendlé <tom.brendle@student.ecp.fr>
2016-09-21 14:55:08 +0200

    Simple login form

---
+++ README.md
@@ -0,0 +1,4 @@
+sudalys
+=======
+
+A Symfony project created on September 21, 2016, 11:34 am.
---
+++ src/AppBundle/Controller/RegistrationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Form\UserType;
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/register", name="user_registration")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $user->setRoles(["ROLE_USER"]);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render(
+            'registration/register.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+}
